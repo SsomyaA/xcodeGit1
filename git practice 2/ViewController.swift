70e24f9af073ff6314f90db61cdc31d63e57878b
Somya <somya.nalco@hotmail.com>
2017-12-05 06:50:47 +0530

    Initial Commit

---
+++ git practice 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git practice 2
+//
+//  Created by Somya Biswal on 05/12/17.
+//  Copyright © 2017 Somya Biswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
